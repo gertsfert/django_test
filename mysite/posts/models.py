e5578a692e7f1508f8fee61bd20deaa18370ccc5
gertsfert <gertsfert@gmail.com>
2018-10-02 09:23:42 +1000

    Added comments model (foriegn key into Posts)

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -11,3 +11,17 @@ class Posts(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comments(models.Model):
+    post = models.ForeignKey('posts.Posts', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    body = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+    
+    def __str__(self):
+        return self.author + " (" + self.created_at + ")"
