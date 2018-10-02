470d8262d6fb91288f74f549a55cae413d091fbf
gertsfert <gertsfert@gmail.com>
2018-10-02 20:55:05 +1000

    add post edit/add page

--- mysite/posts/urls.py
+++ mysite/posts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path(r'details/<id>/', views.details, name='details'),
+    path('new', views.post_new, name='post_new'),
 ]
