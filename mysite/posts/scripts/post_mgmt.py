47793e18fe9f07f145fec7a3ac3582359407c044
gertsfert <gertsfert@gmail.com>
2018-10-04 17:46:59 +1000

    - add scripts folder; post_mgmt.create_copies() to create new posts - added click to requirements (unused yet)

---
+++ mysite/posts/scripts/post_mgmt.py
@@ -0,0 +1,16 @@
+from posts.models import Post
+
+def create_copies(copies: int, title='dummy', body='auto gen from script', published=False):
+    print('attempting to create copies')
+    for n in range(copies):
+        t = title + "_" + str(n)
+
+
+        print('Copy[{}];\n\t \
+            title[{}];\n\t \
+            body[{}];\n\t'.format(n, t, body))
+        p = Post(title=t, body=body)
+        p.save()
+
+        if published:
+            p.publish()
