483a6bd660011e79189711592caf91cb0bfa78a0
gertsfert <gertsfert@gmail.com>
2018-10-07 01:22:30 +1000

    script to add comments to posts

--- mysite/posts/scripts/post_mgmt.py
+++ mysite/posts/scripts/post_mgmt.py
@@ -1,7 +1,8 @@
-from posts.models import Post
+from posts.models import Post, Comment
+from django.shortcuts import get_object_or_404
 
-def create_copies(copies: int, title='dummy', body='auto gen from script', published=False):
-    print('attempting to create copies')
+def create_posts(copies: int, title='dummy', body='auto gen from script', published=False):
+    print('attempting to create posts')
     for n in range(copies):
         t = title + "_" + str(n)
 
@@ -14,3 +15,24 @@ def create_copies(copies: int, title='dummy', body='auto gen from script', publi
 
         if published:
             p.publish()
+
+def create_comments(
+    copies: int, 
+    post_id: int, 
+    author_id:int=1, 
+    body='auto gen from script', 
+    approved=False):
+
+    print('attempting to create comments')
+
+    for n in range(copies):
+        print('Copy[{}];\t \
+            post_id[{}];\n\t \
+            author_id[{}];\n\t \
+            approved[{}];'.format(n, post_id, author_id, approved))
+
+        p = get_object_or_404(Post, id=post_id)
+        c = Comment(post=p, author=author_id, body=body)
+
+        if approved:
+            c.approve()
